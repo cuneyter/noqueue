22b0043509532c8f6dd045dba9bf9c1f39615041
Cuneyt Ergun <cuneytergun@gmail.com>
2021-02-27 12:57:52 +0000

    update the validations and associations

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,2 +1,5 @@
 class Customer < ApplicationRecord
+  has_one :queue_position
+
+  validates :mobile_number, presence: true, numericality: true
 end
--- app/models/queue_position.rb
+++ app/models/queue_position.rb
@@ -1,4 +1,5 @@
 class QueuePosition < ApplicationRecord
   belongs_to :customer
+  validates_uniqueness_of :customer_id
   belongs_to :store
 end
