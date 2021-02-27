1b238f91aa58103b8ecd7019edd1600aa7dda346
Cuneyt Ergun <cuneytergun@gmail.com>
2021-02-27 13:00:39 +0000

    comment out the validations

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,5 +1,5 @@
 class Customer < ApplicationRecord
   has_one :queue_position
 
-  validates :mobile_number, presence: true, numericality: true
+  # validates :mobile_number, presence: true, numericality: true
 end
--- app/models/store.rb
+++ app/models/store.rb
@@ -2,6 +2,6 @@ class Store < ApplicationRecord
   belongs_to :user
   has_many :queue_positions
 
-  validates :store_name, :qr_code, presence: true
-  validates :store_capacity, presence: true, numericality: true
+  # validates :store_name, :qr_code, presence: true
+  # validates :store_capacity, presence: true, numericality: true
 end
