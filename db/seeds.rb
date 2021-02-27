201e5d7cd92876e86fa45010aa249a94bf24bac5
Pawel Dabkowski <paweldabkowski01@gmail.com>
2021-02-27 12:56:57 +0000

    database seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,34 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+# Creating user (shop owner)
+user = User.create(
+    email: "user@user.example",
+    password: "123456"
+)
+puts "User was created."
+
+# Creating users store
+users_store = Store.create(
+    store_name: Faker::Company.name,
+    store_capacity: 20,
+    qr_code: 'test', # need to be changed
+    user_id: user.id
+)
+puts "Store \"#{users_store.store_name}\" was created."
+
+# Creating users and queue positions
+10.times do
+    customer = Customer.create(
+        mobile_number: Faker::Base.numerify('+44 #### ### ###')
+        )
+    customers_queue = QueuePosition.create(
+        customer_id: customer.id,
+        store_id: users_store.id
+        )
+    puts "Customer #{customer.id} provided a phone number \"#{customer.mobile_number}\" and was added as #{customers_queue.id} to the queue."
+end
+puts "Database seeded!"
