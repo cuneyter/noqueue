580a24322a7006b21b86ab82147d106209fc01eb
Cuneyt Ergun <cuneytergun@gmail.com>
2021-03-10 20:18:08 +0000

    add destroy button for each queue position

--- app/controllers/queue_positions_controller.rb
+++ app/controllers/queue_positions_controller.rb
@@ -3,6 +3,10 @@ class QueuePositionsController < ApplicationController
   end
 
   def destroy
+    @queue_position = QueuePosition.find(params[:id])
+    @queue_position.destroy
+
+    redirect_to store_customers_path(@queue_position.store)
   end
 
   def show
