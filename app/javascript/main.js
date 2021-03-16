fe440dc0b9d217f405569b95ba40988dae00470b
Signe Bunke <signebunke@gmail.com>
2021-03-16 21:00:24 +0100

    carousel added to the cards

--- app/javascript/main.js
+++ app/javascript/main.js
@@ -1,8 +1,14 @@
-var swiper = new Swiper('.swiper-container', {
-  slidesPerView: 2,
-  spaceBetween: 30,
-  pagination: {
-    el: '.swiper-pagination',
-    clickable: true,
-  },
-});
+import Swiper from 'swiper';
+
+const initSwiper = () => {
+  var swiper = new Swiper('.swiper-container', {
+    slidesPerView: 2,
+    spaceBetween: 30,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+  });
+}
+
+export {initSwiper}
