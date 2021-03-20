40bc430fc52983df30607a5595e6caa76cd216f2
Pawel Dabkowski <paweldabkowski01@gmail.com>
2021-03-20 14:16:37 +0000

    mobile navbar

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,5 +3,7 @@ class PagesController < ApplicationController
 
   def home
     @stores = Store.all
+    raise
+    @store = Store.all.first # for navbar button "Queues" for demo purpose
   end
 end
